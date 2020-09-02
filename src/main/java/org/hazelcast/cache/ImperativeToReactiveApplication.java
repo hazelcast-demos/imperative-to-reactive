c9d6f102665cb0551a1c9398a282a73832d58ab4
Nicolas Frankel <nicolas@frankel.ch>
2020-09-02 22:37:48 +0200

    Improve readability by using an idiomatic database populator

--- src/main/java/org/hazelcast/cache/ImperativeToReactiveApplication.java
+++ src/main/java/org/hazelcast/cache/ImperativeToReactiveApplication.java
@@ -1,35 +1,31 @@
 package org.hazelcast.cache;
 
-import org.springframework.boot.CommandLineRunner;
+import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
-import org.springframework.util.StreamUtils;
+import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 import reactor.blockhound.BlockHound;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 @SpringBootApplication
 @EnableR2dbcRepositories
 public class ImperativeToReactiveApplication {
 
     @Bean
-    public CommandLineRunner initialize(DatabaseClient client) {
-        return (args) -> {
-            var schemaIn = new ClassPathResource("/schema.sql").getInputStream();
-            var schema = StreamUtils.copyToString(schemaIn, UTF_8);
-            var dataIn = new ClassPathResource("/data.sql").getInputStream();
-            var data = StreamUtils.copyToString(dataIn, UTF_8);
-            client.sql(schema)
-                    .then()
-                    .and(client
-                            .sql(data)
-                            .then())
-                    .block();
-        };
+    public ConnectionFactoryInitializer initialize(ConnectionFactory factory) {
+        var initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(factory);
+        var populator = new CompositeDatabasePopulator();
+        populator.addPopulators(
+                new ResourceDatabasePopulator(new ClassPathResource("/schema.sql")),
+                new ResourceDatabasePopulator(new ClassPathResource("/data.sql"))
+        );
+        initializer.setDatabasePopulator(populator);
+        return initializer;
     }
 
     public static void main(String[] args) {
