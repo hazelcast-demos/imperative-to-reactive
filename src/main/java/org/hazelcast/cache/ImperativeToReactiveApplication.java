c4a90dd2363a489c4577224272b7c62a7234169b
Nicolas Frankel <nicolas@frankel.ch>
2020-09-16 15:36:19 +0200

    Ensure that no call is blocking

--- src/main/java/org/hazelcast/cache/ImperativeToReactiveApplication.java
+++ src/main/java/org/hazelcast/cache/ImperativeToReactiveApplication.java
@@ -3,12 +3,14 @@ package org.hazelcast.cache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 @EnableR2dbcRepositories
 public class ImperativeToReactiveApplication {
 
     public static void main(String[] args) {
+        BlockHound.install();
         SpringApplication.run(ImperativeToReactiveApplication.class, args);
     }
 }
