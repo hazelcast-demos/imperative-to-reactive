115acfd4dbd4390133bca08983f7f4087e065ff1
Nicolas Frankel <nicolas@frankel.ch>
2020-10-28 16:28:30 +0100

    Manually add caching in a naive straightforward way

--- src/main/java/org/hazelcast/cache/ImperativeToReactiveApplication.kt
+++ src/main/java/org/hazelcast/cache/ImperativeToReactiveApplication.kt
@@ -1,14 +1,21 @@
 package org.hazelcast.cache
 
+import com.hazelcast.core.HazelcastInstance
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 import reactor.blockhound.BlockHound
 
 
 @SpringBootApplication
 @EnableR2dbcRepositories
-class ImperativeToReactiveApplication
+class ImperativeToReactiveApplication {
+
+    @Bean
+    fun service(instance: HazelcastInstance, repository: PersonRepository) =
+        CachingService(instance.getMap<Long, Person>("persons"), repository)
+}
 
 fun main() {
     BlockHound.builder()
