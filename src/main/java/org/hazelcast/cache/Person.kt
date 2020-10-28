4a5ee756a5e1e1c3e2312574e332bc4407c7177a
Nicolas Frankel <nicolas@frankel.ch>
2020-10-28 15:14:35 +0100

    Migrate to Spring WebMVC.fn

--- src/main/java/org/hazelcast/cache/Person.kt
+++ src/main/java/org/hazelcast/cache/Person.kt
@@ -2,20 +2,30 @@ package org.hazelcast.cache
 
 import org.hibernate.annotations.Cache
 import org.hibernate.annotations.CacheConcurrencyStrategy
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.domain.Sort
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.function.RouterFunctions.route
+import org.springframework.web.servlet.function.ServerResponse.ok
 import java.time.LocalDate
 import javax.persistence.*
 
-@RestController
-class PersonController(private val repository: PersonRepository) {
 
-    @GetMapping("/person")
-    fun getAll() = repository.findAll(Sort.by("lastName", "firstName"))
+@Configuration
+class PersonRoutes {
 
-    @GetMapping("/person/{id}")
-    fun getOne(@PathVariable id: Long) = repository.findById(id)
+    @Bean
+    fun getAll(repository: PersonRepository) = route()
+        .GET("/person") {
+            ok().body(repository.findAll(Sort.by("lastName", "firstName")))
+        }.build()
+
+    @Bean
+    fun getOne(repository: PersonRepository) = route()
+        .GET("/person/{id}") {
+            req -> ok().body(repository.findById(req.pathVariable("id").toLong()))
+        }.build()
 }
 
 interface PersonRepository : JpaRepository<Person?, Long?>
