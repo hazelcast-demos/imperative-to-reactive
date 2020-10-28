5083d16e2ab5d9fe6c33a25590b1b750ef652a53
Nicolas Frankel <nicolas@frankel.ch>
2020-10-28 15:57:36 +0100

    Replace Spring Data JPA by Spring R2DBC

--- src/main/java/org/hazelcast/cache/ImperativeToReactiveApplication.kt
+++ src/main/java/org/hazelcast/cache/ImperativeToReactiveApplication.kt
@@ -1,10 +1,33 @@
 package org.hazelcast.cache
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.core.io.ClassPathResource
+import org.springframework.r2dbc.core.DatabaseClient
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
+import org.springframework.util.StreamUtils
+
 
 @SpringBootApplication
-class ImperativeToReactiveApplication
+@EnableR2dbcRepositories
+class ImperativeToReactiveApplication {
+
+    @Bean
+    fun initialize(client: DatabaseClient) = CommandLineRunner {
+            val schemaIn = ClassPathResource("/schema.sql").inputStream
+            val schema = StreamUtils.copyToString(schemaIn, Charsets.UTF_8)
+            val dataIn = ClassPathResource("/data.sql").inputStream
+            val data = StreamUtils.copyToString(dataIn, Charsets.UTF_8)
+            client.sql(schema)
+                .then()
+                .and(client
+                    .sql(data)
+                    .then())
+                .block()
+        }
+}
 
 fun main() {
     SpringApplication.run(ImperativeToReactiveApplication::class.java)
